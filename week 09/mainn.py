73be27ff664a0fc1a63c9a8b7106b4aefe99fdbb
Syed Azfar Abbas <119115197+syedazfarabbas235@users.noreply.github.com>
2025-08-30 14:18:00 +0500

    Update mainn.py

--- week 09/mainn.py
+++ week 09/mainn.py
@@ -1,12 +1,17 @@
 from bday_paradox import BirthdayParadox
 
 def main():
-    # Example runs
-    bp1 = BirthdayParadox(group_size=23, simulations=100)
-    bp1.show_probability()
+    try:
+        
+         # Example runs
+        bp1 = BirthdayParadox(group_size=23, simulations=30)
+        bp1.show_probability()
 
-    bp2 = BirthdayParadox(group_size=50, simulations=100)
-    bp2.show_probability()
+        bp2 = BirthdayParadox(group_size=0, simulations=20) #raising error here
+        bp2.show_probability()
+
+    except (TypeError,ValueError) as e:
+        print(f"error:{e}")
 
 if __name__ == "__main__":
     main()
