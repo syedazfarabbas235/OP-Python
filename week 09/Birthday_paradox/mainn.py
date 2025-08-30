c845861ba720826802780ea40f9b74a909cd3dbf
Syed Azfar Abbas <119115197+syedazfarabbas235@users.noreply.github.com>
2025-08-30 18:17:34 +0500

    Add files via upload

---
+++ week 09/Birthday_paradox/mainn.py
@@ -0,0 +1,17 @@
+from bday_paradox import BirthdayParadox
+
+def main():
+    try:
+        
+         # Example runs
+        bp1 = BirthdayParadox(group_size=23, simulations=30)
+        bp1.show_probability()
+
+        bp2 = BirthdayParadox(group_size=0, simulations=20) #raising error here
+        bp2.show_probability()
+
+    except Exception as e:
+        print(f"error:{e}")
+
+if __name__ == "__main__":
+    main()
