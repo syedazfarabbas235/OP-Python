d6f4c1cb74fef4fd3488e62180f7cebb01d06765
Syed Azfar Abbas <119115197+syedazfarabbas235@users.noreply.github.com>
2025-09-30 05:36:30 +0500

    Update main.py

--- week 12/Permutation_generator/main.py
+++ week 12/Permutation_generator/main.py
@@ -1,14 +1,18 @@
 from permutation import PermutationGenerator
 
 def main():
-    # Input string "catdog"
-    generator = PermutationGenerator("catdog")
+    try:
+        # Input string "catdog"
+        generator = PermutationGenerator("catdog")
 
-    # Display all permutations
-    generator.display()
+        # Display all permutations
+        generator.display()
 
-    # Show summary using __str__
-    print(generator)
+        # Show summary using __str__
+        print(generator)
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     main()
